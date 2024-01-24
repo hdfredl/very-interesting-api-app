47ed2d31be6d7a545d385835072d44f1c8866946
hdfredl <dat.leef@gmail.com>
2024-01-24 22:56:46 +0100

    lite stylingg

--- main.js
+++ main.js
@@ -43,9 +43,45 @@ function getFoods() {
     .then((foodData) => displayFoodsToServer(foodData));
 }
 
+// function displayFoodsToServer(foods) {
+//   foods.forEach((foodfromServer) => {
+//     foodContainer.innerHTML += `
+//     <div class ="user col-4">
+//     <img style="width: 200px; height: 200px; object-fit: cover;" src="${foodfromServer.image}"/>,
+//     <p <span class="dish"> Food dish: ${foodfromServer.name}
+//     <p class="points" > Rating: ${foodfromServer.rating}
+//     </div>
+//     `;
+//   });
+//   console.log(foods);
+// }
+
+// another-foodContainer
+
+let anotherFoodContainer = document.querySelector("#anotherfoodContainer");
+
 function displayFoodsToServer(foods) {
   foods.forEach((foodfromServer) => {
-    foodContainer.innerHTML += `<img style="width: 200px; height: 200px; object-fit: cover;" src="${foodfromServer.image}"/>, <p> Name: ${foodfromServer.name}, Rating: ${foodfromServer.rating} `;
+    let html = `
+        <div class="user col-4 mb-3"> 
+        <!-- GÖR RUNDA BORDERS -->
+
+          <div class="card h-100 border rounded"> 
+          <!-- GÖR BILDEN RUNDADD -->
+
+            <img src="${foodfromServer.image}" class="card-img-top img-fluid rounded" style="height: 200px; object-fit: cover;" />
+            <!-- LÄGG IN DOM I EN CARD så dom displayas som i pokemon --> 
+            <div class="card-body">
+              <p class="h5">Food dish: ${foodfromServer.name}</p>
+              <p>Rating: ${foodfromServer.rating}</p>
+              <div class="d-flex justify-content-between">
+                <button class="btn btn-warning" >Update</button>
+                <button class="btn btn-danger" >Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    anotherFoodContainer.innerHTML += html;
   });
-  console.log(foods);
 }
